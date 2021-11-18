d1264ad81860edace8f084fa0b900bc6bf9e0cc0
T1ma <webdevtima@gmail.com>
2021-11-18 16:38:01 +0200

    created the structure of the educational project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return (
+        <div className='app-info'>
+            <h1>Учет отличников в групе ІСТ</h1>
+            <h2>Общее число студентов: </h2>
+            <h3>Стипендию получат: </h3>
+        </div>
+    )
+}
+
+export default AppInfo;
